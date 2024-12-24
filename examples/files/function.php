e2d5cde97979e6bef795d2bc6e62a7abfe04d172
Dmitrii Derepko <xepozz@list.ru>
2024-12-24 17:17:54 +0300

    add namespaced functions

--- examples/files/function.php
+++ examples/files/function.php
@@ -1,7 +1,20 @@
 <?php
 
-render('views/index.php');
 
-function render(string $view)
-{
+namespace {
+    function render(string $view)
+    {
+    }
+}
+
+
+namespace App {
+    function render(string $view)
+    {
+    }
+}
+
+namespace Usage {
+    render('views/index.php');
+    \App\render('views/index.php');
 }
