95fa31a3ddb14d54985b2d4a56b367880e6d1d89
Dmitrii Derepko <xepozz@list.ru>
2024-12-30 18:21:22 +0300

    add more examples

--- examples/collections/container.php
+++ examples/collections/container.php
@@ -5,7 +5,7 @@ namespace App {
     use Framework\Container;
 
     $c = new Container;
-    $command = $c->getCommand('app');
+    $command = $c->getCommand('');
     $tag = $c->getTag('tag_logger');
 
 }
---
+++ examples/methods/tables.php
@@ -0,0 +1,19 @@
+<?php
+
+class MethodFinder {}
+
+MethodFinder::getTables('sqlite_master');
+
+class ActiveRecord {
+    public static function tableName()
+    {
+        return '';
+    }
+
+
+    public function method1()
+    {
+        return '';
+    }
+}
+
