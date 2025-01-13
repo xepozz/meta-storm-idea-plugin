51893fec877048ec0248e1f74c6e353c367a104e
Dmitrii Derepko <xepozz@list.ru>
2025-01-13 15:40:21 +0300

    more examples

--- examples/injection/lang.php
+++ examples/injection/lang.php
@@ -4,10 +4,14 @@ class LangControl {
     public function regexp(string $value){}
     public function css(string $value){}
     public function html(string $value){}
+    public function sql(string $value){}
+    public function php(string $value){}
 }
 
 $control = new LangControl();
 $control->regexp('[a-z]add+');
 $control->css('div.class{display: flex;}');
 $control->html('div*5');
+$control->sql('select 1');
+$control->php('<?= "string" . random_int(1, 2);');
 $control->htmla('div*5');
