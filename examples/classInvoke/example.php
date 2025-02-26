d3cef1081b95879041ca6159183286db360faded
Dmitriy Derepko <xepozz@list.ru>
2025-02-26 10:56:05 +0300

    updates

--- examples/classInvoke/example.php
+++ examples/classInvoke/example.php
@@ -2,7 +2,7 @@
 
 namespace Callable {
     class Invokable {
-        public function __invoke(string $name){}
+        public function __invoke(string $name, string $value=''){}
         public function method1(string $name){}
         public function method2(string $name){}
         public function method3(string $name){}
@@ -11,4 +11,7 @@ namespace Callable {
 
     $obj = new Invokable();
     $obj('method3');
+    $obj(name: 'method3');
+    $obj(name: 'invokable', value: 'test');
+    $obj(value: 'test', name: 'method3');
 }
